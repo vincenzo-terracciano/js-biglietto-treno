a040286800a678b2d787054fe97df061444e950c
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-02-10 16:00:05 +0100

    add if and else in the sale section

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 /* Tools 
-    - prompt per chiedere all'utente il numero di km da percorrere
-    - prompt per chiedere l'età del passeggero  
+    - prompt per chiedere all'utente il numero di km da percorrere;
+    - prompt per chiedere l'età del passeggero;
 */
 
 const user_km = prompt("Scegliere il numero di km da percorrere");
 console.log(user_km);
+
+const user_age = prompt("Inserire l'età del passeggero");
+console.log(user_age);
+
+const ticket_price = user_km * 0.21;
+console.log(ticket_price);
+
+let ticket;
+
+if (user_age < 18) {
+    ticket = ticket_price * 0,8;
+    
+} else if (user_age > 65) {
+    ticket = ticket_price * 0,6;
+    
+} else {
+    ticket = ticket_price
+}
+
+console.log(ticket);
+
+
 
