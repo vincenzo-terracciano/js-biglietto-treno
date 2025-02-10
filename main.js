6f2914df669b36e30e5b7854f7f6a999f46eb818
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-02-10 15:15:14 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+/* Tools 
+    - prompt per chiedere all'utente il numero di km da percorrere
+    - prompt per chiedere l'età del passeggero  
+*/
+
+const user_km = prompt("Scegliere il numero di km da percorrere");
+console.log(user_km);
+
