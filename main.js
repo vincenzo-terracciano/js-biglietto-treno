056a07788ec520b031f96c69e201afa54f8d27c9
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-02-10 16:42:13 +0100

    add number function to transform all string in number

--- main.js
+++ main.js
@@ -3,25 +3,25 @@
     - prompt per chiedere l'età del passeggero;
 */
 
-const user_km = prompt("Scegliere il numero di km da percorrere");
+const user_km = Number(prompt("Scegliere il numero di km da percorrere"));
 console.log(user_km);
 
-const user_age = prompt("Inserire l'età del passeggero");
+const user_age = Number(prompt("Inserire l'età del passeggero"));
 console.log(user_age);
 
-const ticket_price = (user_km * 0.21).toFixed(2);
+const ticket_price = Number((user_km * 0.21).toFixed(2));
 console.log(ticket_price);
 
 let ticket;
 
 if (user_age < 18) {
-    ticket = (ticket_price * 0.8).toFixed(2);
+    ticket = Number((ticket_price * 0.8).toFixed(2));
     
 } else if (user_age > 65) {
-    ticket = (ticket_price * 0.6).toFixed(2);
+    ticket = Number((ticket_price * 0.6).toFixed(2));
     
 } else {
-    ticket = (ticket_price).toFixed(2)
+    ticket = Number((ticket_price).toFixed(2));
 }
 
 console.log(ticket);
