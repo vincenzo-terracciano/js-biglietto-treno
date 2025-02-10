10deb9c148c2a444e5511f2b7e632c55724588f2
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-02-10 16:31:10 +0100

    fix if and else in sale section

--- main.js
+++ main.js
@@ -9,19 +9,19 @@ console.log(user_km);
 const user_age = prompt("Inserire l'età del passeggero");
 console.log(user_age);
 
-const ticket_price = user_km * 0.21;
+const ticket_price = (user_km * 0.21).toFixed(2);
 console.log(ticket_price);
 
 let ticket;
 
 if (user_age < 18) {
-    ticket = ticket_price * 0,8;
+    ticket = (ticket_price * 0.8).toFixed(2);
     
 } else if (user_age > 65) {
-    ticket = ticket_price * 0,6;
+    ticket = (ticket_price * 0.6).toFixed(2);
     
 } else {
-    ticket = ticket_price
+    ticket = (ticket_price).toFixed(2)
 }
 
 console.log(ticket);
